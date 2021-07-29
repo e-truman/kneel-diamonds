91aa5a4f852a04d37ee4929aa6677ca0c9e3769e
Erin Truman <erintruman@Erins-MacBook-Pro.local>
2021-07-29 15:32:40 -0500

    my even listeners are working

---
+++ scripts/Metals.js
@@ -0,0 +1,41 @@
+import { getMetals, setMetal } from "./database.js"
+
+const metals = getMetals()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "metal") {
+
+            setMetal(parseInt(event.target.value))
+            // event.target.checked = true
+            // document.dispatchedEvent( new CustomEvent("state changed"))
+        }
+    }
+)
+
+export const Metals = () => {
+    let html = "<ul>"
+
+    // This is how you have been converting objects to <li> elements
+    for (const metal of metals) {
+
+
+
+        // if(metal.id === ordeBuilder.metalId())
+        html += `<li>
+            <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
+        </li>`
+            
+//     } else {
+//         html += `<li>
+//         <input type = "radio" name = "metal" value="${metal.id}
+//         </li>`
+//     }
+}
+
+    html += "</ul>"
+    // console.log(orderBuilder.matalId)
+    return html
+}
+
